e077f6837dab7d96f96bae88d1d2c2c02c5f7758
Aurelien Cobb <ajmgithub@gmail.com>
2019-09-06 09:39:44 +0100

    Initial Commit

---
+++ Yoox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yoox
+//
+//  Created by Aurelien Cobb on 06/09/2019.
+//  Copyright © 2019 Aurelien Cobb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
